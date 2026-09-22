1564db109fa22d0cf632e84dd44231441171e747
encerOS <ch51s@outlook.com>
2026-09-22 14:02:01 -0600

    Ajuste en el sistema selector de niveles

--- Scenes/Levels/level_selection.gd
+++ Scenes/Levels/level_selection.gd
@@ -1,19 +1,12 @@
 extends Node2D
 
-func _on_level_1_pressed() -> void:
-	GameManager._load_level(str(1))
+func _ready() -> void:
+	var buttons = $LevelsButtonManager.get_children()
+	for i in buttons.size():
+		buttons[i].pressed.connect(_on_level_button_pressed.bind(i + 1))
 
-func _on_level_2_pressed() -> void:
-	GameManager._load_level(str(2))
-
-func _on_level_3_pressed() -> void:
-	GameManager._load_level(str(3))
-
-func _on_level_4_pressed() -> void:
-	GameManager._load_level(str(4))
-
-func _on_level_5_pressed() -> void:
-	GameManager._load_level(str(5))
+func _on_level_button_pressed(level_number: int) -> void:
+	GameManager._load_level(str(level_number))
 
 func _on_return_pressed() -> void:
 	LevelTransition.change_scene_to("res://Scenes/Levels/main_menu.tscn")
